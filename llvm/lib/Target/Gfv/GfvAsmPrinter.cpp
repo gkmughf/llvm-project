05f2dfb6642739b1d2032483753c4a54bae0a621
Fyodor Gorelov <fvgorelov@gmail.com>
2025-04-14 12:25:26 +0300

    [Gfv] 8. Add empty GfvAsmPrinter

---
+++ llvm/lib/Target/Gfv/GfvAsmPrinter.cpp
@@ -0,0 +1,61 @@
+#include "MCTargetDesc/GfvInstPrinter.h"
+#include "Gfv.h"
+#include "GfvSubtarget.h"
+#include "GfvTargetMachine.h"
+#include "TargetInfo/GfvTargetInfo.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+#include "llvm/CodeGen/MachineConstantPool.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCStreamer.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+ 
+using namespace llvm;
+ 
+#define DEBUG_TYPE "asm-printer"
+ 
+namespace {
+ 
+  class GfvAsmPrinter : public AsmPrinter {
+    const MCSubtargetInfo *STI;
+ 
+  public:
+    explicit GfvAsmPrinter(TargetMachine &TM,
+			   std::unique_ptr<MCStreamer> Streamer)
+      : AsmPrinter(TM, std::move(Streamer)), STI(TM.getMCSubtargetInfo()) {
+      GFV_DUMP_GREEN
+    }
+ 
+    void emitInstruction(const MachineInstr *MI) override;
+ 
+    StringRef getPassName() const override { return "Gfv Assembly Printer"; }
+ 
+    bool lowerPseudoInstExpansion(const MachineInstr *MI, MCInst &Inst);
+  };
+ 
+} // end anonymous namespace
+ 
+// Gfvple pseudo-instructions have their lowering (with expansion to real
+// instructions) auto-generated.
+#include "GfvGenMCPseudoLowering.inc"
+ 
+void GfvAsmPrinter::emitInstruction(const MachineInstr *MI) {
+  GFV_DUMP_GREEN
+  // Do any auto-generated pseudo lowerings.
+  if (MCInst OutInst; lowerPseudoInstExpansion(MI, OutInst)) {
+    EmitToStreamer(*OutStreamer, OutInst);
+    return;
+  }
+}
+ 
+// Force static initialization.
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeGfvAsmPrinter() {
+  RegisterAsmPrinter<GfvAsmPrinter> X(getTheGfvTarget());
+}
