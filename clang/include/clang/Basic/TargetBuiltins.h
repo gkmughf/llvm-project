124d90f0a0ca20aab344e42f4f9704f320195c8d
Fyodor Gorelov <fvgorelov@gmail.com>
2025-04-29 23:37:17 +0300

    [Gfv] 25. Add Gfv ClangBuiltin support (flush, putpixel and rand)

--- clang/include/clang/Basic/TargetBuiltins.h
+++ clang/include/clang/Basic/TargetBuiltins.h
@@ -402,12 +402,22 @@ namespace clang {
     };
   }
 
+  /// Gfv builtins
+  namespace Gfv {
+    enum {
+        LastTIBuiltin = clang::Builtin::FirstTSBuiltin-1,
+#define BUILTIN(ID, TYPE, ATTRS) BI##ID,
+#include "clang/Basic/BuiltinsGfv.def"
+        LastTSBuiltin
+    };
+  }
+  
   static constexpr uint64_t LargestBuiltinID = std::max<uint64_t>(
       {ARM::LastTSBuiltin, AArch64::LastTSBuiltin, BPF::LastTSBuiltin,
        PPC::LastTSBuiltin, NVPTX::LastTSBuiltin, AMDGPU::LastTSBuiltin,
        X86::LastTSBuiltin, VE::LastTSBuiltin, RISCV::LastTSBuiltin,
        Hexagon::LastTSBuiltin, Mips::LastTSBuiltin, XCore::LastTSBuiltin,
-       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin});
+       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin, Gfv::LastTSBuiltin});
 
 } // end namespace clang.
 
--- clang/lib/Basic/Targets/Gfv.cpp
+++ clang/lib/Basic/Targets/Gfv.cpp
@@ -1,9 +1,16 @@
 #include "Gfv.h"
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)					\
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsGfv.def"
+};
 
 void GfvTargetInfo::getTargetDefines(const LangOptions &Opts,
                                      MacroBuilder &Builder) const {
@@ -11,5 +18,6 @@ void GfvTargetInfo::getTargetDefines(const LangOptions &Opts,
 }
 
 ArrayRef<Builtin::Info> GfvTargetInfo::getTargetBuiltins() const {
-  return std::nullopt;
+  return llvm::ArrayRef(BuiltinInfo,
+			clang::Gfv::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
